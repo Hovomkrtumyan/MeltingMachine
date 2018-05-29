9a4ccbba766e3d464bcff2b1c17f35bb09a93eb0
Mkrtumyan Hovhannes <hovo.workingon@outlook.com>
2018-05-29 20:45:12 +0400

    First commit

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 const electron = require('electron');
 const path = require('path');
+var Chart = require('chart.js');
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
