a73b4e31c2de6ee1441cfeecac54f77b353d4b4a
Mkrtumyan Hovhannes <hovo.workingon@outlook.com>
2018-05-31 11:49:48 +0400

    First commit

--- app/test.js
+++ app/test.js
@@ -1,6 +1,7 @@
 //var Chart = require('chart.js');
 
 var ctx = document.getElementById('myChart').getContext('2d');
+var maxlength=10;
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
@@ -21,6 +22,14 @@ setInterval(delay, 1000);
 function delay(x) {
     myChart.data.datasets[0].data.push(Math.floor(Math.random() * 50));
     myChart.data.datasets[1].data.push(Math.floor(Math.random() * 50));
+    newdata:myChart.data.datasets[0].data;
+    if(myChart.data.datasets[0].data.length > maxlength) {
+        myChart.data.datasets[0].data.shift();
+    };
+    if(myChart.data.datasets[1].data.length > maxlength) {
+        myChart.data.datasets[1].data.shift();
+    };
     myChart.data.labels.push(new Date().getSeconds());
+    
     myChart.update();
 }
